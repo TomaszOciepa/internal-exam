3a597cf9256f66fee5d48ef7619763c4b0cd4c2c
tomaszociepa <tom.ociepa@gmail.com>
2019-04-08 20:21:32 +0200

    extends of the indexServlet class

--- src/main/java/servlet/IndexServlet.java
+++ src/main/java/servlet/IndexServlet.java
@@ -1,4 +1,23 @@
 package servlet;
 
-public class IndexServlet {
+import freemarker.TemplateProvider;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/exam")
+public class IndexServlet extends HttpServlet {
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
