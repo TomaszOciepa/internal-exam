7c41024517ad169e564bd6a849faaca1bc0e9347
tomaszociepa <tom.ociepa@gmail.com>
2019-04-09 21:31:54 +0200

    add index servlet

--- src/main/java/servlet/IndexServlet.java
+++ src/main/java/servlet/IndexServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -9,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(urlPatterns = "/exam")
 public class IndexServlet extends HttpServlet {
@@ -18,6 +23,16 @@ public class IndexServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Content-Type", "text/html; charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        Template template;
+        Map<String, Object> model = new HashMap<>();
 
+        template = templateProvider.getTemplate(getServletContext(), "index");
+        try {
+            template.process(model, out);
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
     }
 }
